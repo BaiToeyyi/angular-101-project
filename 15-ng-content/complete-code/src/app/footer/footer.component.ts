acd57840e99fe8ca4721c97b4dab72a3ad508ab7
supagrid <boyisboyis.work@gmail.com>
2019-02-02 03:07:15 +0700

    feat: update code

--- 15-ng-content/complete-code/src/app/footer/footer.component.ts
+++ 15-ng-content/complete-code/src/app/footer/footer.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, ViewContainerRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterContentInit {
 
+  @ContentChild('contentFooter', { read: ViewContainerRef }) contentFooter: ViewContainerRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    console.log(this.contentFooter);
   }
 
 }
